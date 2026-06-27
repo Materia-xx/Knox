3c2535b3404ea0d39fd76849155b6c40de6fbd2a
Materia <170386+Materia-xx@users.noreply.github.com>
2026-06-27 10:24:53 -0700

    add sample android version

---
+++ Knox.Android/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Knox.Android;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Knox.Android/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Knox.Android;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Tapped {count} time \u2014 it works!";
+		else
+			CounterBtn.Text = $"Tapped {count} times \u2014 it works!";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
